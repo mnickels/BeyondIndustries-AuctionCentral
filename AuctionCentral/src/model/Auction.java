163a4e692b0c4f805c997e9c597c8d72c4a1de63
lamminghoi <lamminghoi@gmail.com>
2016-11-10 18:50:53 -0800

    Commited From Ming Auction.java and AuctionTest.java is added.

---
+++ AuctionCentral/src/model/Auction.java
@@ -0,0 +1,222 @@
+package model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Auction class stores all the information of an auction, which includes
+ * the nonprofit info., date of auction, name of auction, description, and list
+ * of the items from the auction. When an item is added to the auction, it will
+ * make sure no same item will appear twice in the auction item list. When an item
+ * is removed, it will make sure the item exists in the list in order to be removed.
+ * 
+ * @author Ming Lam
+ * @version 11/09/2016
+ */
+public class Auction implements Serializable{
+
+	/**
+	 * Default UID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The info. of the nonprofit
+	 */
+	private Nonprofit myNonprofit;
+	
+	/**
+	 * A list of the items
+	 */
+	private List<Item> myItems;
+	
+	/**
+	 * The date of the auction
+	 */
+	private LocalDateTime myDate;
+	
+	/**
+	 * The name of the auction
+	 */
+	private String myName;
+	
+	/**
+	 * The number of items in the auction
+	 */
+	private int size;
+	
+	/**
+	 * The auction's description
+	 */
+	private String myDescription;
+	
+	/**
+	 * The constructor of the Auction class
+	 * 
+	 * @param theNonprofit info. of the nonprofit
+	 * @param theDate date of the auction
+	 * @param theName name of the auction
+	 * @param theDescription aution's description
+	 */
+	Auction(Nonprofit theNonprofit, LocalDateTime theDate, String theName, String theDescription) {
+		myNonprofit = theNonprofit;
+		myDate = theDate;
+		myName = theName;
+		myDescription = theDescription;
+		
+		myItems = new ArrayList<Item>();
+		size = 0;
+	}
+	
+	/**
+	 * get the nonprofit info
+	 * 
+	 * @return myNonprofit
+	 */
+	public Nonprofit getNonprofit() {
+		return myNonprofit;
+	}
+	
+	/**
+	 * set the nonprofit info
+	 * 
+	 * @param theNonprofit nonprofit info
+	 */
+	public void setNonprofit(Nonprofit theNonprofit) {
+		myNonprofit = theNonprofit;
+	}
+	
+	/**
+	 * get the date of the auction
+	 * 
+	 * @return myDate
+	 */
+	public LocalDateTime getDate() {
+		return myDate;
+	}
+	
+	/**
+	 * set the date of the auction
+	 * 
+	 * @param theDate date of auction
+	 */
+	public void setDate(LocalDateTime theDate) {
+		myDate = theDate;
+	}
+	
+	/**
+	 * get the name of the auction
+	 * 
+	 * @return myName
+	 */
+	public String getName() {
+		return myName;
+	}
+	
+	/**
+	 * set the name of the auction
+	 * 
+	 * @param theName name of auction
+	 */
+	public void setName(String theName) {
+		myName = theName;
+	}
+	
+	/**
+	 * get the auction's description
+	 * 
+	 * @return myDescription
+	 */
+	public String getDescription() {
+		return myDescription;
+	}
+	
+	/**
+	 * set the auction's description
+	 * 
+	 * @param theDescription auction's description
+	 */
+	public void setDescription(String theDescription) {
+		myDescription = theDescription;
+	}
+	
+	/**
+	 * get the list of items that are in the auction
+	 * 
+	 * @return myItems
+	 */
+	public List<Item> getItems() {
+		return myItems;
+	}
+	
+	/**
+	 * get the number of items in the auction 
+	 * 
+	 * @return size;
+	 */
+	public int getSize() {
+		return size;
+	}
+	
+	/**
+	 * add a new item to the auction
+	 * 
+	 * @param theItem new item to be added
+	 */
+	public boolean addItem(Item theItem) {
+		boolean result = true;
+		
+		for (int i = 0; i < size; i++) {
+			if (theItem.getName().equals(myItems.get(i).getName())) {
+				result = false;
+				break;
+			}
+		}
+		
+		if (result) {
+			myItems.add(theItem);
+			size++;
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * remove an item from the auction
+	 * 
+	 * @param theItem the item that will be removed
+	 */
+	public boolean removeItem(Item theItem) {
+		boolean result = false;
+		
+		for (int i = 0; i < size; i++) {
+			if (theItem.getName().equals(myItems.get(i).getName())) {
+				myItems.remove(i);
+				size--;
+				result = true;
+				break;
+			}
+		}
+		
+
+		return result;
+	}
+	
+	public String toString() {
+		String result = "Auction: ";
+		result += myName;
+		result += "\nOrganization: ";
+		result += myNonprofit.getOrganizationName();
+		result += "\nDate: ";
+		result += myDate;
+		result += "\nNumber of items: ";
+		result += size;
+		result += "\nDescription: ";
+		result += myDescription;
+		
+		return result;
+		
+	}
+}
---
+++ AuctionCentral/src/model/AuctionTest.java
@@ -0,0 +1,123 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author Ming Lam
+ * @version 11/09/2016
+ */
+public class AuctionTest {
+
+	Auction myAuction;
+	
+	Nonprofit myNonprofit;
+	
+	Item myItem;
+	
+	@Before
+	public void setUp() {
+		myItem = new Item("Onion Ring", "Mrs. Field", 10, "New", "Small", "999 Deadend Road",
+				new BigDecimal(3.99), "Tasty snack that has never been opened.");
+		myNonprofit = new Nonprofit("Santa Claus", "sclaus", "scalus@email", "2533522525");
+
+		LocalDateTime date = LocalDateTime.of(2016, 11, 9, 11, 00);
+		myAuction = new Auction(myNonprofit, date, "Onion Auction", "This is a boring auction that only sell onion rings.");
+	}
+
+	@Test
+	public void testGetNonprofit() {
+		assertEquals(myNonprofit, myAuction.getNonprofit());
+	}
+
+	@Test
+	public void testSetNonprofit() {
+		Nonprofit  testNonprofit = new Nonprofit("Harry Potter", "hpotter", "hpotter@email", "2532532532");
+		myAuction.setNonprofit(testNonprofit);
+		assertEquals(testNonprofit, myAuction.getNonprofit());
+	}
+
+	@Test
+	public void testGetDate() {
+		LocalDateTime testDate = LocalDateTime.of(2016, 11, 9, 11, 00);
+		assertEquals(testDate, myAuction.getDate());
+	}
+
+	@Test
+	public void testSetDate() {
+		LocalDateTime testDate = LocalDateTime.of(2017, 10, 10, 11, 00);
+		myAuction.setDate(testDate);
+		assertEquals(testDate, myAuction.getDate());
+	}
+
+	@Test
+	public void testGetName() {
+		assertEquals("Onion Auction", myAuction.getName());
+	}
+
+	@Test
+	public void testSetName() {
+		myAuction.setName("Potato Auction");
+		assertEquals("Potato Auction", myAuction.getName());
+	}
+
+	@Test
+	public void testGetDescription() {
+		assertEquals("This is a boring auction that only sell onion rings.", myAuction.getDescription());
+	}
+
+	@Test
+	public void testSetDescription() {
+		myAuction.setDescription("This is an interesting auction that only sell potato.");
+		assertEquals("This is an interesting auction that only sell potato.", myAuction.getDescription());
+	}
+
+
+	@Test
+	public void testGetSize() {
+		assertEquals(0, myAuction.getSize());
+	}
+
+	public void testAddItem() {
+		assertTrue(myAuction.addItem(myItem));
+	}
+	
+	@Test
+	public void testAddItemOnSameItemTwice() {
+		myAuction.addItem(myItem);
+		
+		Item testItem = new Item("Onion Ring", "Mickey Mouse", 5, "New", "Small", "111 Deadend Road",
+				new BigDecimal(3.99), "Tasty snack that has never been opened."); 
+		assertFalse(myAuction.addItem(testItem));
+	}
+
+	@Test
+	public void testRemoveItem() {
+		myAuction.addItem(myItem);
+
+		Item testItem = new Item("Onion Ring", "Mrs. Field", 10, "New", "Small", "999 Deadend Road",
+				new BigDecimal(3.99), "Tasty snack that has never been opened.");
+		assertTrue(myAuction.removeItem(testItem));
+
+	}
+	
+	@Test
+	public void testRemoveItemOnNotExistItem() {		
+		Item testItem = new Item("Onion Ring", "Mrs. Field", 10, "New", "Small", "999 Deadend Road",
+				new BigDecimal(3.99), "Tasty snack that has never been opened.");
+		assertFalse(myAuction.removeItem(testItem));
+	}
+
+	@Test
+	public void testToString() {
+		String testOutput = "Auction: Onion Auction\nOrganization: null\nDate: 2016-11-09T11:00"
+				+ "\nNumber of items: 0\nDescription: This is a boring auction that only sell onion rings.";
+		assertEquals(testOutput, myAuction.toString());
+	}
+}
