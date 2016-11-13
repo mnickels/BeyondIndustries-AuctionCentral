fce8dc359dec44b6bfbf71daed5c6bdcc010f942
Ian Richards <Ian Richards>
2016-11-13 13:24:10 -0800

    Fixed toString methods to be more useful

--- AuctionCentral/src/model/users/Bidder.java
+++ AuctionCentral/src/model/users/Bidder.java
@@ -55,15 +55,7 @@ public class Bidder extends Account {
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(getEmail());
-		sb.append('\n');
-		sb.append(getName());
-		sb.append('\n');
-		sb.append(getPhoneNumber());
-		sb.append('\n');
-		sb.append(getUsername());
-		sb.append('\n');
-		sb.append(getAddress());
+		sb.append("Bidder");
 		return sb.toString();
 	}
 
