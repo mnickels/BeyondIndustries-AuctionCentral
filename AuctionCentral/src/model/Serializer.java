916dcc2af40a65c144fc9725a2d4340041aaaa78
geostojanov <gestojanov@gmail.com>
2016-11-14 22:03:30 -0800

    Changes to Serializer and DataTests Added main method to serializer to write an sample instance of Data

--- AuctionCentral/src/model/Serializer.java
+++ AuctionCentral/src/model/Serializer.java
@@ -12,6 +12,13 @@ import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import model.users.Account;
+import model.users.Bidder;
+import model.users.Nonprofit;
+import model.users.Staff;
 
 /**
  * Reads and Writes Serializable Objects to and from files.
@@ -52,4 +59,55 @@ public final class Serializer {
 		}
 		return obj;
 	}
+	
+	public static void main(String[] args) {
+		
+		Data d = Data.getInstance();
+		
+		for(int i = 0; i < 26; i++) {
+			Account np = new Nonprofit("Nonprofit " + i, "username", "email", "phonenumber", LocalDateTime.MIN, "FreePuppies");
+			d.addUser(np.getName(), np);
+		}
+		
+		for(int i = 0; i < 4; i++) {
+			Account b = new Bidder("Bidder " + i, "username", "email", "phonenumber", "address");
+			d.addUser(b.getName(), b);
+		}
+		
+		for(int i = 0; i < 3; i++) {
+			Account s = new Staff("Staff " + i, "username", "email", "phonenumber");
+			d.addUser(s.getName(), s);
+		}
+		
+		for(int i = 0; i < 24; i++) {
+			
+			Auction auc = new Auction((Nonprofit) d.getUser("Nonprofit " + i), 
+					d.getCurrentDateTime().plusWeeks(1).plusDays(i), 
+					"AuctionName" + i, "AuctionDescr");
+			
+			auc.addItem(new Item("Item 1", "Donor 1", 1, "new", "medium", "address", 
+					BigDecimal.valueOf(50), "description"));
+			
+			auc.addItem(new Item("Item 2", "Donor 2", 1, "like new", "large", "address", 
+					BigDecimal.valueOf(50), "description"));
+			
+			auc.addItem(new Item("Item 3", "Donor 3", 1, "used", "medium", "address", 
+					BigDecimal.valueOf(50), "description"));
+			
+			
+			d.addAuction(auc);
+			
+			
+		}
+		
+		for(Auction a: d.getAuctions()) {
+			System.out.println(a.getName() + " " + a.getNonprofit().getName());
+		}
+		
+		writeFile(d, "dataInstance24Auctions.ser");
+		
+		System.out.println("Done!");
+		
+	}
+	
 }
